c8e97a02bcd071b8d0dcf945618c136134996e82
Omer Khan <okhanlodhi@gmail.com>
2020-02-09 22:18:14 -0600

    Finished project + comments

--- Project1/Project1/A_star_8_puzzle.py
+++ Project1/Project1/A_star_8_puzzle.py
@@ -4,82 +4,18 @@
 
 import operator
 
-class States:
-    def __init__(self,data,level,fvalue,huer, parent):
-    # Constructor to set data of the node, level of the node and the fvalue
-        self.data = data
-        self.level = level
-        self.fvalue = fvalue
-        self.huer = huer
-        self.parent = parent
-
-    def compare(self, state):
-        for j in state:
-            if( self.data == j.data):
-                return True
-        return False
-    
-    def generate_child_states(self):
-    # Generate the children nodes of any given node by moving the blank space up, down, left, right
-        x,y = self.find(self.data,'0')
-    # value_list contains position values for moving the blank space up, down, left, right
-        value_list = [[x, y-1],[x, y+1],[x-1, y],[x+1, y]]
-        childrens = []
-        for i in value_list:
-            child = self.possible_states(self.data,x,y,i[0],i[1])
-            if child is not None:
-                child_states = States(child,self.level+1,0,0,self)
-                childrens.append(child_states)
-        return childrens
-        
-    def possible_states(self,puzzle,x1,y1,x2,y2):
-        # Move the blank space in the possible direction and if the position value is out of place, then return None
-        if x2 >= 0 and x2 < len(self.data) and y2 >= 0 and y2 < len(self.data):
-            temp_puzzle = []
-            temp_puzzle = self.copy(puzzle)
-            temp = temp_puzzle[x2][y2]
-            temp_puzzle[x2][y2] = temp_puzzle[x1][y1]
-            temp_puzzle[x1][y1] = temp
-            return temp_puzzle
-        else:
-            return None
-        
-    def copy(self,root):
-        # create a copy matrix of given state
-        temp = []
-        for i in root:
-            t = []
-            for j in i:
-                t.append(j)
-            temp.append(t)
-        return temp    
-            
-    def find(self,puzzle,x):
-        # Find the [i][j] position of the empty space
-        for i in range(0,len(self.data)):
-            for j in range(0,len(self.data)):
-                if puzzle[i][j] == x:
-                    return i,j
+# class puzzle for puzzle manipulation and processing
 class Puzzle:
     def __init__(self):
-        # Contructor initializes puzzle size and open and closed lists
+        # contructor initializes puzzle size and open and closed lists
         self.n = 3
         self.open = []
         self.closed = []
-        
-    def accept(self):
-        # Accepts the puzzle from the user 
-        puzzle = []
-        for i in range(0,self.n):
-            temp = input().split(" ")
-            puzzle.append(temp)
-        return puzzle
     
     def h(self,state,goal):
-        # Calculate distance difference
+        # calculate the manhattan distance of tile
         temp = 0
         temp2 = 0
-        #print()
         for i in range(0,self.n):
             for j in range(0,self.n):
                 if state[i][j] != goal[i][j] and state[i][j] != '0':
@@ -91,6 +27,7 @@ class Puzzle:
         return temp2
 
     def loadFile(self):
+    # load and read file that contains the random puzzle starting states
         givenPuzzles = []
         with open('program_1_data.txt', 'r') as fn:
             for line in fn:
@@ -106,15 +43,21 @@ class Puzzle:
         return givenPuzzles
 
     def checkSolvable(self,Puzzle):
+    # check if the random puzzle state is solvable by using the mathematical way of 'inversion'
+    # even inversion = solvable, odd inversion = unsolvable
         inversion = 0
-        for i in range(len(Puzzle)):                            # current x location of the tile
-            for j in range(len(Puzzle[0])):                     # current y location of the tile     
-                for k in range(i,len(Puzzle)):                 # x location of comparison tile
+        # current x location of the tile
+        for i in range(len(Puzzle)):
+            # current y location of the tile
+            for j in range(len(Puzzle[0])):
+                # x location of comparison tile     
+                for k in range(i,len(Puzzle)):
                     if k == i:
                         start = j
                     else:
                         start = 0
-                    for l in range(start,len(Puzzle[0])):          # y location of comparison tile
+                    # y location of comparison tile
+                    for l in range(start,len(Puzzle[0])):          
                         if int(Puzzle[i][j]) > int(Puzzle[k][l]) and int(Puzzle[k][l]) != 0:
                             inversion += 1
         if inversion%2 == 1:
@@ -122,6 +65,7 @@ class Puzzle:
         return True
 
     def solvePath(self, end):
+    # saves the path from puzzle start state to goal state, if puzzle is solvable
         path = []
         temp = end
         while(temp !=0):
@@ -135,10 +79,12 @@ class Puzzle:
             print()
     
     def process(self, Puzzles):
+    # main process of the program
+    # inputs the puzzles, and checks if each puzzle is solvable or not
         for i in range (0,len(Puzzles)):
             self.open = []
             self.closed = []
-            print("Press Enter to do Puzzle ",i)
+            print("Press Enter to do Puzzle",i+1)
             input()
             start = Puzzles[i]
             for x in start:
@@ -167,7 +113,8 @@ class Puzzle:
                             self.open.append(i)
                     self.closed.append(self.open[0])
                     del self.open[0]
-                    
+
+                    # sort the open list based on manhattan distance
                     self.open.sort(key=operator.attrgetter("fvalue","huer"), reverse=False)
                     if(len(self.open) == 0):
                         print('NO SOLUTIONS')
@@ -180,7 +127,67 @@ class Puzzle:
                 print("\n--------------------------")
             else:
                 print("Not Solvable\n--------------------------")
+
+# class states that handle manpulation of the different states of a given puzzle
+class States:
+    def __init__(self,data,level,fvalue,huer, parent):
+    # constructor to set data of the node, level of the node, fvalue for a state, huer to calcualte fvalue, and parent state
+        self.data = data
+        self.level = level
+        self.fvalue = fvalue
+        self.huer = huer
+        self.parent = parent
+
+    def compare(self, state):
+    # compares the current state to a saved state
+        for j in state:
+            if( self.data == j.data):
+                return True
+        return False
     
+    def generate_child_states(self):
+    # generate the children nodes of any given node by moving the blank space up, down, left, right
+        x,y = self.find(self.data,'0')
+        # value_list contains position values for moving the blank space up, down, left, right
+        value_list = [[x, y-1],[x, y+1],[x-1, y],[x+1, y]]
+        childrens = []
+        for i in value_list:
+            child = self.possible_states(self.data,x,y,i[0],i[1])
+            if child is not None:
+                child_states = States(child,self.level+1,0,0,self)
+                childrens.append(child_states)
+        return childrens
+        
+    def possible_states(self,puzzle,x1,y1,x2,y2):
+        # check for validity when moving the blank space
+        if x2 >= 0 and x2 < len(self.data) and y2 >= 0 and y2 < len(self.data):
+            temp_puzzle = []
+            temp_puzzle = self.copy(puzzle)
+            temp = temp_puzzle[x2][y2]
+            temp_puzzle[x2][y2] = temp_puzzle[x1][y1]
+            temp_puzzle[x1][y1] = temp
+            return temp_puzzle
+        else:
+            return None
+        
+    def copy(self,root):
+        # create a copy matrix of given state
+        temp = []
+        for i in root:
+            t = []
+            for j in i:
+                t.append(j)
+            temp.append(t)
+        return temp    
+            
+    def find(self,puzzle,x):
+        # find the [i][j] position of the empty space
+        for i in range(0,len(self.data)):
+            for j in range(0,len(self.data)):
+                if puzzle[i][j] == x:
+                    return i,j
+
+# create a puzzle object, load the file, and process the puzzles in file
 newPuzzle = Puzzle()
 Puzzles = newPuzzle.loadFile() 
 newPuzzle.process(Puzzles)
