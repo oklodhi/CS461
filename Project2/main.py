72302a267e459525074591777af444a571ceecd0
Omer Khan <okhanlodhi@gmail.com>
2020-02-11 15:32:34 -0600

    initialize project 2

---
+++ Project2/main.py
@@ -0,0 +1,18 @@
+# Omer Khan
+# Project 2
+# CS 461 - Brian Hare
+
+
+
+courses = [['CS101A','40'],['CS101B','25'],['CS201A','30'],['CS201B','30'],['CS191A','60'],['CS191B','20'],['CS291A','20'],['CS291B','40'],['CS303','50'],['CS341','40'],['CS449','55'],['CS461','40']]
+timing = ['10A','11A','12P','1P','2P','3P','4P']
+classrooms = [['Haag 301','70'],['Haag 206','30'],['Royall 204','70'],['Katz 209','50'],['Flarsheim 310','80'],['Flarsheim 260','25'],['Bloch 0009','30']]
+professors = {
+    "Hare" : ['CS101A','CS101B','CS201A','CS201B','CS291A','CS291B','CS303','CS449','CS461'],
+    "Bingham" : ['CS101A','CS101B','CS201A','CS201B','CS191A','CS191B','CS291A','CS291B','CS449'],
+    "Kuhail" : ['CS303','CS341'],
+    "Mitchell" : ['CS191A','CS191B','CS291A','CS291B','CS303','CS341'],
+    "Rao" : ['CS291A','CS291B','CS303','CS341','CS461'],
+    "Staff" : ['CS101A','CS101B','CS201A','CS201B','CS191A','CS191B','CS291A','CS291B','CS303','CS341','CS449','CS461']
+    }
+
